227dbfc973a2d2f748864035ba6d48f8a8b278ba
Vishul <apple@MacBook-Pro.local>
2026-05-18 17:09:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className="min-h-screen flex items-center justify-center bg-black text-white">
+        <div className="text-center">
+          <h1 className="text-6xl font-bold">
+            Vishul
+          </h1>
+
+          <p className="text-2xl mt-4 text-gray-400">
+            Full Stack Developer
+          </p>
+
+          <button className="mt-6 px-6 py-3 bg-white text-black rounded-lg">
+            Download Resume
+          </button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
